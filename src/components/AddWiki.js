6925ddc21b8d81bc32076df307d740c42b4ebe66
Linn <linn.johansson7@hotmail.com>
2019-10-29 17:12:52 +0100

    display wikiItems and add new with form

---
+++ src/components/AddWiki.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import app from "../firebase";
+import { useAuth } from "../context/auth";
+
+const StyledAddWiki = styled.div``;
+
+const AddWiki = () => {
+  const db = app.firestore();
+  const [wikiWord, setWikiWord] = useState("");
+  const [wikiDescription, setWikiDescription] = useState("");
+  // eslint-disable-next-line no-unused-vars
+  //   const [wikiCategory, setWikiCategory] = useState("");
+
+  // Get user if logged in
+  const { authUser } = useAuth();
+
+  // If any fields are empty disable submit-button
+  const isInvalid = wikiWord === "" || wikiDescription === "";
+
+  const addNewWiki = async e => {
+    e.preventDefault();
+
+    // Validate and format data here:
+
+    if (authUser) {
+      // Add data to firebase here:
+      db.collection("vintageWiki")
+        .doc(wikiWord)
+        .set({
+          name: wikiWord,
+          description: wikiDescription,
+          addedByUser: authUser.email
+        })
+        .then(function() {
+          console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+    }
+  };
+
+  return (
+    <StyledAddWiki>
+      <input
+        type="text"
+        name="wikiWord"
+        id="wikiWord"
+        placeholder="Lägg till nytt ord i VintageWiki"
+        value={wikiWord}
+        onChange={e => setWikiWord(e.target.value)}
+      />
+      <input
+        type="text"
+        name="wikiDescription"
+        id="wikiDescription"
+        placeholder="Lägg till beskrivning av ordet."
+        value={wikiDescription}
+        onChange={e => setWikiDescription(e.target.value)}
+      />
+      <button type="submit" onClick={addNewWiki} disabled={isInvalid}>
+        Lägg till
+      </button>
+    </StyledAddWiki>
+  );
+};
+
+export default AddWiki;
--- src/pages/vintageWiki.js
+++ src/pages/vintageWiki.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import app from "../firebase";
+import { useAuth } from "../context/auth";
+import AddWiki from "../components/AddWiki";
 
 const StyledVintageWiki = styled.div``;
 
+const StyledItem = styled.div`
+  border: solid 1px black;
+`;
+
 const VintageWiki = () => {
+  const db = app.firestore();
+  const [wiki, setWiki] = useState([]);
+
+  // Get user if logged in
+  const { authUser } = useAuth();
+
+  useEffect(() => {
+    const tempArray = [];
+
+    db.collection("vintageWiki")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          tempArray.push(doc.data());
+        });
+        setWiki(tempArray);
+      });
+  }, []);
+
   return (
     <StyledVintageWiki>
       <h2>Här kommer det bli en VintageWiki.</h2>
+      {wiki.map(item => (
+        <StyledItem key={item}>
+          <h3>{item.name}</h3>
+          <h4>{item.description}</h4>
+        </StyledItem>
+      ))}
+
+      {authUser ? (
+        <AddWiki />
+      ) : (
+        <h4>Logga in för att kunna lägga till nya ord i VintageWikin.</h4>
+      )}
     </StyledVintageWiki>
   );
 };
