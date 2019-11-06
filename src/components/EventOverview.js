bc2bf952c2af2de4442695006003f0470e4e4331
Linn <linn.johansson7@hotmail.com>
2019-11-06 10:00:01 +0100

    make eventOverview expandable

--- src/components/EventOverview.js
+++ src/components/EventOverview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 // import { start } from "repl";
 
@@ -10,6 +10,7 @@ const EventOverview = event => {
   const data = event.event;
   const startTimestamp = data.startDate.seconds;
   const endTimestamp = data.endDate.seconds;
+  const [expanded, setExpanded] = useState(false);
 
   function timeConverter(timestamp) {
     const a = new Date(timestamp * 1000);
@@ -17,18 +18,39 @@ const EventOverview = event => {
     const month = a.getMonth();
     const date = a.getDate();
     const hour = a.getHours();
-    const min = a.getMinutes();
-    const time = `${date}/${month}-${year} ${hour}:${min}`;
-    return time;
+    const min = `0${a.getMinutes()}`.slice(-2);
+    const eventDate = `${date}/${month}-${year}`;
+    const time = `${hour}:${min}`;
+    return `${eventDate} Klockan: ${time}`;
   }
 
   return (
     <StyledEventOverview>
-      <h3>{data.name}</h3>
-      <h4>Startar: {timeConverter(startTimestamp)}</h4>
-      <h4>Slutar: {timeConverter(endTimestamp)}</h4>
-      <h4>Plats: {data.city}</h4>
-      <p>{data.description}</p>
+      {!expanded ? (
+        <div>
+          <h3>{data.name}</h3>
+          <h4>{timeConverter(startTimestamp)}</h4>
+          <h4>Plats: {data.city}</h4>
+          <button type="button" onClick={() => setExpanded(true)}>
+            Visa mer
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>{data.name}</h3>
+          <h4>Startar: {timeConverter(startTimestamp)}</h4>
+          <h4>Slutar: {timeConverter(endTimestamp)}</h4>
+          <h4>Plats: {data.address}</h4>
+          <h4>Stad: {data.city}</h4>
+          <p>{data.description}</p>
+          <h4>
+            Extern länk: <a href={data.link}>{data.link}</a>
+          </h4>
+          <button type="button" onClick={() => setExpanded(false)}>
+            Visa mindre
+          </button>
+        </div>
+      )}
     </StyledEventOverview>
   );
 };
