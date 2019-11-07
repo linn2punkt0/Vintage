c021d6104cec44b3bc776ba9083af366409d32d7
Linn <linn.johansson7@hotmail.com>
2019-11-07 11:40:48 +0100

    add some placeholder content

--- src/pages/materials.js
+++ src/pages/materials.js
@@ -7,6 +7,17 @@ const Materials = () => {
   return (
     <StyledMaterials>
       <h2>Här kan du läsa på om material och skötselråd.</h2>
+
+      <h3>Materialguide</h3>
+      <p>
+        Här ska det finnas bild på materialet, beskrivning/info, och råd om hur
+        man tvättar och sköter om det på bästa sätt.
+      </p>
+      <h3>Klädvård</h3>
+      <p>
+        Här ska det finnas andra råd kring hur du tar hand om dina kläder, lagar
+        dem m.m.
+      </p>
     </StyledMaterials>
   );
 };
