319f9539e98acf0fd524ee9c8938dbe33a7a7e05
Linn <linn.johansson7@hotmail.com>
2019-11-07 14:47:45 +0100

    filter by date asc/desc and region

--- src/pages/events.js
+++ src/pages/events.js
@@ -7,17 +7,29 @@ import AddEvents from "../components/AddEvents";
 
 const StyledEvents = styled.div``;
 
+const FilterBlock = styled.div``;
+
 const Events = () => {
   const db = app.firestore();
+  // fetch andset once
+  const [regions, setRegions] = useState([]);
+
+  // Update many times
   const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [regionFilter, setRegionFilter] = useState("");
+  const [dateOrder, setDateOrder] = useState("");
 
   // Get user if logged in
   const { authUser } = useAuth();
 
+  // Get all events, sort by date
   useEffect(() => {
     const tempArray = [];
+    const order = dateOrder || "asc";
 
     db.collection("events")
+      .orderBy("startDate", order)
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
@@ -25,17 +37,111 @@ const Events = () => {
         });
         setEvents(tempArray);
       });
+  }, [dateOrder]);
+
+  // get all regions
+  useEffect(() => {
+    const tempArray = [];
+
+    db.collection("regions")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          tempArray.push({ id: doc.id, ...doc.data() });
+        });
+        setRegions(tempArray);
+      });
   }, []);
+
+  // get events by timeperiod ------ WORK IN PROGRESS
+  // useEffect(() => {
+  //   const tempArray = [];
+
+  //   db.collection("events")
+  //     .where("timeperiods", "array-contains", "50-tal")
+  //     .get()
+  //     .then(querySnapshot => {
+  //       querySnapshot.forEach(doc => {
+  //         tempArray.push({ id: doc.id, ...doc.data() });
+  //       });
+  //       setEvents(tempArray);
+  //     });
+  // }, []);
+
+  // useEffect(() => {
+  //   const tempArray = [];
+
+  //   db.collection("events")
+  //     .where("timeperiod", "array-contains", "50-tal")
+  //     .get()
+  //     .then(querySnapshot => {
+  //       querySnapshot.forEach(doc => {
+  //         tempArray.push({ id: doc.id, ...doc.data() });
+  //       });
+  //       setFilteredEvents(tempArray);
+  //     });
+  // }, []);
+
+  // get all events where region == regionFilter
+  useEffect(() => {
+    const tempArray = [];
+
+    db.collection("events")
+      .where("region", "==", regionFilter)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          tempArray.push({ id: doc.id, ...doc.data() });
+        });
+        setFilteredEvents(tempArray);
+      });
+  }, [regionFilter]);
+
+  console.log(`filter: ${regionFilter}`);
+
+  filteredEvents.forEach(e => {
+    console.log(`resultat: ${e.name}`);
+  });
 
   return (
     <StyledEvents>
-      <h2>Här hittar du alla events!</h2>
-      {events.map(event => (
-        <EventOverview key={event.id} event={event} />
-      ))}
+      <h2>Här hittar du alla event!</h2>
+      <FilterBlock>
+        <select
+          type="text"
+          name="dateOrder"
+          id="dateOrder"
+          placeholder="Sortera datum"
+          // value={dateOrder}
+          onChange={e => setDateOrder(e.target.value)}
+        >
+          <option value="asc">Sortera efter datum</option>
+          <option value="desc">Fallande</option>
+          <option value="asc">Stigande</option>
+        </select>
+        <select
+          type="text"
+          name="region"
+          id="region"
+          placeholder="Region"
+          value={regionFilter}
+          onChange={e => setRegionFilter(e.target.value)}
+        >
+          {regions.map(region => (
+            <option key={region.id} value={region.name}>
+              {region.name}
+            </option>
+          ))}
+        </select>
+      </FilterBlock>
+      {filteredEvents.length > 0
+        ? filteredEvents.map(event => (
+            <EventOverview key={event.id} event={event} />
+          ))
+        : events.map(event => <EventOverview key={event.id} event={event} />)}
 
       {authUser ? (
-        <AddEvents />
+        <AddEvents regions={regions} />
       ) : (
         <h4>Logga in för att kunna lägga till nya events.</h4>
       )}
